ab1bdc895ec5677f9b9211aa0d10b8a145e4a16c
gtrofino <guitrofino@gmail.com>
2026-01-15 15:02:51 -0300

    ci(fix): substitute release configuration by .cjs file

---
+++ release.config.cjs
@@ -0,0 +1,113 @@
+module.exports = {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        writerOpts: {
+          commitPartial:
+            '- {{commitDate}} {{commitUrl}} {{#if scope}}**{{scope}}:** {{/if}}{{subject}}\n',
+
+          commitGroupsSort: 'title',
+          commitsSort: ['scope', 'subject'],
+          groupBy: 'type',
+
+          transform: function (commit, context) {
+            // create a shallow clone of commit
+            const c = Object.assign({}, commit);
+
+            switch (commit.type) {
+                case 'feat':
+                    c.type = 'Features';
+                    break;
+                case 'fix':
+                    c.type = 'Bug Fixes';
+                    break;
+                case 'perf':
+                    c.type = 'Performance Improvements';
+                    break;
+                case 'docs':
+                    c.type = 'Documentation';
+                    break;
+                case 'style':
+                    c.type = 'Styles';
+                    break;
+                case 'refactor':
+                    c.type = 'Code Refactoring';
+                    break;
+                case 'test':
+                    c.type = 'Tests';
+                    break;
+                case 'chore':
+                    c.type = 'Maintenance';
+                    break;
+                case 'ci':
+                    c.type = 'Continuos Integration';
+                    break;
+
+                default:
+                    return;
+            }
+
+            // clone references array
+            if (Array.isArray(commit.references)) {
+              c.references = commit.references.slice();
+            }
+
+            // shortHash formated as 7 characters
+            if (commit.hash) {
+                c.shortHash = commit.hash.substring(0, 7);
+            } else {
+                c.shortHash = '';
+            }
+            
+            // commitUrl formatted as Markdown
+            if (c.shortHash) {
+                c.commitUrl = `([${c.shortHash}](${context.host}/${context.owner}/${context.repository}/commit/${commit.hash}))`
+            }
+
+            // commitDate formatted as YYYY-MM-DD
+            if (commit.committerDate) {
+              const d = new Date(commit.committerDate);
+
+              c.commitDate = isNaN(d.getTime()) ? '' : d.toISOString().slice(0, 10);
+            } else {
+              c.commitDate = '';
+            }
+
+            // header fallback
+            c.header = commit.header || commit.subject || '';
+
+            return c;
+          }
+        }
+      }
+    ],
+
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          {
+            path: '*.{sty,tex}',
+            name: 'TeX-${nextRelease.gitTag}',
+            label:
+              'TeX source files required for compiling the PDF file on ${nextRelease.gitTag}.'
+          },
+          {
+            path: '*.pdf',
+            name: 'PDF-${nextRelease.gitTag}',
+            label: 'Curriculum Vitae compiled PDF file on ${nextRelease.gitTag}'
+          }
+        ],
+        failComment: false,
+        labels: ['bug', 'maintenance'],
+        releasedLabels: false,
+        successComment: false
+      }
+    ]
+  ]
+};
